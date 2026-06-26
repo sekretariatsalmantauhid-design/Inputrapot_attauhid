1f610951758148df2474806c4560ae3be4570b44
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-06-26 15:12:17 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH ANGKA INI SETIAP KALI UPDATE asatidzah.html (Misal: v2, v3, v4)
-const CACHE_NAME = 'erapot-guru-v1.3'; 
+const CACHE_NAME = 'erapot-guru-v1.4'; 
 
 const urlsToCache = [
   './',
